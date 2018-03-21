9bc8025dcdbe26c5100d4103a7c7bf052c43224e
Reynard Catindig <Reynard@DWEXT70.tmagroup.local>
2018-03-21 08:46:58 +0800

    commit the asp.net files

---
+++ TMAAdmin/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TMAAdmin.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        }
+    }
+}
